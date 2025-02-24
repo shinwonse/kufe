6aca55127ff93cfd512d1576f508e2787f56f01a
신원세/프론트엔드개발팀 <wsshin@ktown4u.com>
2025-02-24 17:59:30 +0900

    Add file-system based routing with @react-router/fs-routes

---
+++ app/routes/_index.tsx
@@ -0,0 +1,12 @@
+import type { Route } from "./+types/_index";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: 'KUFE - Home' },
+    { name: 'description', content: 'Welcome to KUFE!' },
+  ];
+}
+
+export default function Home() {
+  return <div>Home</div>;
+}
